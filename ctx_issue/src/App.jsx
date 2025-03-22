ca38f7286f04959ca86463c96e0a2bf717219b37
AsifZaman777 <eramasif16@gmail.com>
2025-03-22 16:47:54 +0600

    ctx_issue project created

---
+++ ctx_issue/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='text-center'>
+      <h1 className='text-2xl font-semibold m-5 bg-slate-300 p-2'>CTX menu rerender issue</h1>
+    </div>
+  );
+};
+
+export default App;
