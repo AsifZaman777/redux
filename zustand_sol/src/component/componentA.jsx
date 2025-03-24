ee6ac39fd540ead97e031c32cbc4328e96e9382a
AsifZaman777 <eramasif16@gmail.com>
2025-03-24 10:44:57 +0600

    zustand implementation

--- zustand_sol/src/component/componentA.jsx
+++ zustand_sol/src/component/componentA.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { use } from "react";
+import useCounterStore from "../store/useCounterStore";
 
 const componentA = () => {
-
-    return (
-        <div className="bg-blue-100 border p-4 rounded shadow-md">
-            <h1 className="text-blue-700 text-lg font-bold">Component A</h1>
-            <p className="text-blue-600">Counter: <span className="text-xl font-mono"></span></p>
-        </div>
-    );
+  const count = useCounterStore((state) => state.count);
+  console.log("Component A rendered");
+  return (
+    <div className="bg-blue-100 border p-4 rounded shadow-md">
+      <h1 className="text-blue-700 text-lg font-bold">Component A</h1>
+      <p className="text-blue-600">
+        Counter: <span className="text-xl font-mono">{count}</span>
+      </p>
+    </div>
+  );
 };
 
 export default componentA;
--- zustand_sol/src/component/componentB.jsx
+++ zustand_sol/src/component/componentB.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
+import useCounterStore from "../store/useCounterStore";
 
 const componentB = () => {
-
-    return (
-        <div className="bg-green-100 border p-4 rounded shadow-md">
-            <h1 className="text-green-700 text-lg font-bold">Component B</h1>
-            <button
-           
-                className="bg-green-500 rounded text-white hover:bg-green-600 mt-2 px-4 py-2 transition"
-            >
-                Increment Counter
-            </button>
-            <button
-          
-                className="bg-red-500 rounded text-white hover:bg-green-600 mt-2 px-4 py-2 transition ml-2"
-            >
-                Decrement Counter
-            </button>
-        </div>
-    );
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+  console.log("Component B rendered");
+  return (
+    <div className="bg-green-100 border p-4 rounded shadow-md">
+      <h1 className="text-green-700 text-lg font-bold">Component B</h1>
+      <button
+        onClick={increment}
+        className="bg-green-500 rounded text-white hover:bg-green-600 mt-2 px-4 py-2 transition"
+      >
+        Increment Counter
+      </button>
+      <button
+        onClick={decrement}
+        className="bg-red-500 rounded text-white hover:bg-green-600 mt-2 px-4 py-2 transition ml-2"
+      >
+        Decrement Counter
+      </button>
+    </div>
+  );
 };
 
 export default componentB;
